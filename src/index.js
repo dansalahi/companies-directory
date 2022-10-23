61af087c89e04e6bc6843b25b7681cf3be2791d6
daniel salahi <dan.salahii@gmail.com>
2022-10-23 13:02:46 +0300

    initialize the project: webpack, tailwindcss

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('we are using webpack.')
+
+
+const hello = () => { console.log('hello from webpack!') }
+
+
+hello()
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  watch: true,
+  entry: './src/index.js',
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "dist")
+  }
+}
